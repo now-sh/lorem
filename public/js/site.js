358e442196713f41a62176c80c5324847175860b
Jason <git-admin@casjaysdev.com>
2021-12-02 18:29:36 -0500

    🗃 Modified: package.json public/js/site.js views/index.ejs  🗃

--- public/js/site.js
+++ public/js/site.js
@@ -1,20 +1,24 @@
-$( function() {
-
+$(function () {
   function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
-  $('[data-name="generate"]').click( function() {
+  $('[data-name="generate"]').click(function () {
     var paragraphs = $('[data-name="paragraphs"]').val();
     var sentences = $('[data-name="sentences"]').val();
     var ptags = $('[data-name="p-tags"]').val();
 
-    if(!isNumber(paragraphs) || !isNumber(sentences) || paragraphs <= 0 || sentences <= 0) {
+    if (
+      !isNumber(paragraphs) ||
+      !isNumber(sentences) ||
+      paragraphs <= 0 ||
+      sentences <= 0
+    ) {
       $('#error').show();
       return;
     }
 
-    if(ptags == 1) {
+    if (ptags == 1) {
       ptags = '?p=true';
     } else {
       ptags = '';
@@ -23,16 +27,18 @@ $( function() {
     window.location = '/paragraphs/' + paragraphs + '/' + sentences + ptags;
   });
 
-  $('[data-name="title"]').click( function() {
-    $.ajax('/sentences/4').done( function(metaphor) {
+  $('[data-name="title"]').click(function () {
+    $.ajax('/sentences/4').done(function (metaphor) {
       $('.metaphor-text').html(metaphor);
     });
   });
 
-  $('[data-name="title"]').hover( function() {
-    $('.metaphor-text').toggleClass('text-red', true);
-  }, function() {
-    $('.metaphor-text').toggleClass('text-red', false);
-  });
-
+  $('[data-name="title"]').hover(
+    function () {
+      $('.metaphor-text').toggleClass('text-red', true);
+    },
+    function () {
+      $('.metaphor-text').toggleClass('text-red', false);
+    }
+  );
 });
